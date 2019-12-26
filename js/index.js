e7b5f43ac990c14e52dafcdd7a0f91b8da7c6741
Ayerss <276956101@qq.com>
2019-12-26 09:17:36 +0800

    feat: 缓存配置功能

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const KEY = '__bookConf';
+
 function getDomText () {
   return new Promise(function (resolve, reject) {
     chrome.tabs.getSelected(null, function (tab) {
@@ -39,6 +41,23 @@ function domTest() {
   });
 }
 
+
+function saveConf() {
+  chrome.storage.local.set({
+    [KEY]: [
+      document.getElementById('titleDom').value,
+      document.getElementById('conDom').value,
+      document.getElementById('nextDom').value,
+    ]
+  });
+}
+
+function clearConf () {
+  chrome.storage.local.set({
+    [KEY]: Array(3).fill('')
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   
   document.getElementById('get2next').addEventListener('click', getToNext, false);
@@ -47,4 +66,15 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('.badge').forEach(dom => {
     dom.addEventListener('click', domTest, false);
   });
+  
+  document.getElementById('saveConf').addEventListener('click', saveConf, false);
+  document.getElementById('clearConf').addEventListener('click', clearConf, false);
+  
+  chrome.storage.local.get(KEY, function(res) {
+    const val = res[KEY] || Array(3).fill('');
+    document.getElementById('titleDom').value = val[0];
+    document.getElementById('conDom').value = val[1];
+    document.getElementById('nextDom').value = val[2];
+  });
+  
 }, false);
